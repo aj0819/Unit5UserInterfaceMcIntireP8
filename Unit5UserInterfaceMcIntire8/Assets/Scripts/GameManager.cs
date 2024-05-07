11a0a0d3ae5a3847c70aa2f01f4a53f697954393
Adam McIntire <757770@student.dusd.net>
2024-05-07 15:24:30 -0700

    Setting up GameOver

--- Unit5UserInterfaceMcIntire8/Assets/Scripts/GameManager.cs
+++ Unit5UserInterfaceMcIntire8/Assets/Scripts/GameManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    private int score;
     private float spawnRate = 1.0f;
 
 
@@ -12,6 +16,8 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -29,5 +35,16 @@ public class GameManager : MonoBehaviour
             Instantiate(targets[index]);
         }
        
+    }
+
+   public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
     }
 }
